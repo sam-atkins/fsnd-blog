f2c75cd0f428bc35d9ee930721c261b312ea393b
Sam Atkins <sam@cubi.io>
2017-01-04 09:35:44 +0000

    feat: Baseline set-up
    - Python code to render html
    - html rendered with simple h1 title
    - yaml with config for templates & static files

---
+++ main.py
@@ -0,0 +1,43 @@
+# [START imports]
+import os
+import webapp2
+import jinja2
+
+from google.appengine.ext import db
+# [END imports]
+
+
+# [START template mgmt & BaseHandler]
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
+                               autoescape=True)
+
+
+class BaseHandler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+# [END template mgmt & BaseHandler]
+
+
+# [START Main Page]
+class MainPage(BaseHandler):
+    def render_main(self):
+        self.render("main.html")
+
+    def get(self):
+        self.render_main()
+# [END Main Page]
+
+
+# [START app]
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
+# [END app]
