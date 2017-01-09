16b0d73483a46bbb1896fbe5bc8aa41fa22039e4
Sam Atkins <sam@cubi.io>
2017-01-09 14:40:51 +0000

    feat: Add basic signup form, no cookies

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import os
 import webapp2
 import jinja2
+import re
 
 from google.appengine.ext import db
 # [END imports]
@@ -107,10 +108,79 @@ class PostPage(BaseHandler):
 # [END Permalink post page]
 
 
+# [START Sign-up]
+
+# Regex for form error handling
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+USER_PW = re.compile(r"^.{3,20}$")
+USER_EM = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+
+
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+
+def valid_password(password):
+    return password and USER_RE.match(password)
+
+
+def valid_email(email):
+    return not email or USER_EM.match(email)
+
+
+class SignUp(BaseHandler):
+    def get(self):
+        self.render("signup-form.html")
+
+    def post(self):
+        have_error = False
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify = self.request.get('verify')
+        email = self.request.get('email')
+
+        params = dict(username=username,
+                      email=email)
+
+        if not valid_username(username):
+            params['error_username'] = "That's not a valid username."
+            have_error = True
+
+        if not valid_password(password):
+            params['error_password'] = "That wasn't a valid password."
+            have_error = True
+        elif password != verify:
+            params['error_verify'] = "Your passwords didn't match."
+            have_error = True
+
+        if not valid_email(email):
+            params['error_email'] = "That's not a valid email."
+            have_error = True
+
+        if have_error:
+            self.render('signup-form.html', **params)
+        else:
+            self.redirect('/welcome?username=' + username)
+# [END Sign-up]
+
+
+# [START Welcome]
+class Welcome(BaseHandler):
+    def get(self):
+        username = self.request.get('username')
+        if valid_username(username):
+            self.render('/welcome.html', username=username)
+        else:
+            self.redirect('/signup-form.html')
+# [END Welcome]
+
+
 # [START app]
 app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/newpost', NewPost),
     ('/([0-9]+)', PostPage),
+    ('/signup-form', SignUp),
+    ('/welcome', Welcome),
 ], debug=True)
 # [END app]
