864b6b81bc22a5ff188321f8d817a98c182d06de
Sam Atkins <sam@cubi.io>
2017-01-20 10:45:27 +0000

    style: Remove code comment

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from myapp.models import *
 app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/newpost', NewPost),
-    ('/([0-9]+)', LikePage),  # PostPage old value
+    ('/([0-9]+)', LikePage),
     ('/edit/post/([0-9]+)', EditPost),
     ('/delete/post/([0-9]+)', DeletePost),
     ('/comment/([0-9]+)', Comment),
